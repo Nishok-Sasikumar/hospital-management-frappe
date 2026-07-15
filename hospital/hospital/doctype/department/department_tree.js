3a62c14fa9701875fb2c858d3e826f47eb2f190b
Nishok Sasikumar <nishok2005kuro@gmail.com>
2026-07-15 12:40:50 +0000

    Added validation for hospital Settings

---
+++ hospital/hospital/doctype/department/department_tree.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2026, Nishok and contributors
+// For license information, please see license.txt
+
+// frappe.treeview_settings["Department"] = {
+// };
--- hospital/hospital/doctype/hospital_settings/hospital_settings.py
+++ hospital/hospital/doctype/hospital_settings/hospital_settings.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2026, Nishok and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import validate_email_address
 
-
+# class HospitalSettings(Document):
+#  	pass
 class HospitalSettings(Document):
-	pass
+	def before_save(self):
+		if self.hospital_name:
+			self.hospital_name = self.hospital_name.title()
+		else:
+			frappe.throw("Hospital Name is required.")
+		if self.phone:
+			if len(self.phone) != 14:
+				frappe.throw("Phone Number must be a 14-digit number.")
